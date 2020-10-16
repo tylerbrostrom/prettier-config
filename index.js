1cbd53fa776e2edc22bf155d532474bb059ae22f
Tyler Brostrom <brstrm@gmail.com>
2020-10-16 01:58:28 -0500

    Init project

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    jsxSingleQuote: true,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5'
+}
